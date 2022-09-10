107e55ce817911b4dc89469cf81d498fe5e803ab
RocioRodriguez1618 <108986007+RocioRodriguez1618@users.noreply.github.com>
2022-09-10 16:53:40 -0500

    Actividad3

---
+++ index.js
@@ -0,0 +1,358 @@
+
+// console.log();
+
+// //comemntario de  linea
+// fglugg
+// kjgo
+// hoh
+
+// /* comentario de bloque
+// dyfufulfl 
+// hfouygñgñg 
+// foguñghñ
+// */
+
+// //Definicionaes de variable o aignacion
+// nombre = "andres"
+
+// let edad=5;
+
+// const EDAD = 5;
+// EDAD = 6; 
+
+// let numeroUnoBonito
+
+// let numeroUno = 3;
+
+// let numeroDos = 2;
+// let suma= numeroUno + numeroDos;
+// console.loh();
+
+// let saludo = "Hola";
+// let nombre = "Gonza";
+// let mensaje = saludo + = = + nombre;
+// console.log("------aca se puede comentar----", mensaje);
+
+
+// numero_uno_bonito
+
+// let edad = 10;
+
+// let nombre = "andres";
+
+// console.log("Bienvenido señor " + nombre + " tienes " + edad + " de edad");
+
+// let nombre = prompt("ingresa el nombre");
+// console.log("Hola " + nombre);    // es un strim 
+// let nombre = prompt("ingresa el nombre");
+// let mensaje = "Bienvenido@ " + nombre;
+// let edad = "Bienvenido@ " + nombre + " de edad";
+
+// alert(mensaje);
+// let nombre = "andres";
+
+// if(nombre === "andres"){
+//     console.log("entramos");
+
+// }
+// console.log("terminamos");
+// let nombre = prompt("ingrese su nombre").toLocaleUpperCase();
+
+// if (nombre === "felipe"){
+
+// }
+// alert("terinammos");
+
+
+// /* estructura if else */
+
+//  let nombre = prompt("ingresa el nombre").toLowerCase();
+
+//  if(nombre === "andres")
+// let nombre = prompt("ingrese su nombre").toLowerCase();
+
+// if(nombre === "andrés"){
+//   alert("Hola Felipe");
+// }else{
+//   alert("quien eres?");
+// }
+
+// alert("terminamos");
+
+// let respuesta = prompt("terminaste la tarea?");
+
+// if(respuesta === "si"){
+
+// }
+
+// estructura del if else ifif(conicion)
+
+// {
+//     codigo a ejecutar cuando la condicion es verdadera 
+// }
+
+// let edad = parseInt(prompt("Ingrese su edad"));
+
+// if(edad < 15){
+// alert("No puedes entrar");
+// }else if(edad < 18){
+//     alert("Puede entrar con un adulto");
+// }else{
+// alert("puede entrar a la fista");    
+// }
+// let number = prompt( "ingrese un numero");
+
+// if(number > 1000){
+//   alert("es mayotr a mil");
+// }else {
+//   alert("es menor a 1000");
+// } 
+
+
+/* repetir codigo  estructura dell foe*/
+// for(AudioDestinationNode; HashChangeEvent; actualizacion){
+//     codigo a repartir 
+// // }
+// for(let i = 10; i < 100;  i = i + 5){
+//     console.log(i);
+// }
+
+
+/* for(let i = 0; i <= 10; i++){
+  if(i === 5){
+    break;
+  }
+
+  console.log(i);
+}
+
+console.log("terminamos"); */
+
+/* for(let i = 0; i <= 10; i++){
+  if(i === 5){
+    continue;
+  }
+
+  console.log(i);
+}
+
+console.log("terminamos"); */
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i === 5) {
+//         break;
+//     }
+
+//     console.log(i);
+// }
+
+// console.log("terminamos");
+
+//  for(let i = 0; i <= 10; i++){
+//   if(i === 5){
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// console.log("terminamos"); 
+
+
+
+// while(entrada != "ESC" )
+//    switch (entrada) {
+//        case "NO PUEDES INGRESAR":
+//             alert("NIVEL NO COMPLETADO");
+//             break;
+//         case "NO PUEDES INGRESAR":
+//             alert("NIVEL NO COMPLETADO");
+//             break;
+//        default:
+//            alert("BIENVENIDO NIVEL COMPLETADO")
+//            break;
+//    }
+
+// calcular costo total de productos selecionado por usuario
+//calcular pagos en cuotas cobr un monto determinado 
+//cACULAR VALOR FINAL DE UN PRODUCYO SELECIONADO EN FUCNCIN DE IMPUESTOS Y DESCUENTOS 
+//CALUCLAR TIEMPO DE ESPERA PROMEDIO SEN RELACION YE¡N CANTIFDAD Y TINRNOS REGISTRADOS 
+//CACULAR EDAD PROMEDIO DE PEROSNAS REGISTRADAS 
+//CAC UÑAR NOTA DINAL DE ALUMNOS INGRESADOS 
+
+// let nombre = prompt('PARA CONTINUAR INGRESE SU NOMBRE: ');
+// confirm('Hola ' + nombre + '!');
+
+// let ejecutar = true;
+// let num1 = 0;
+// let num2 = 0;
+// while (ejecutar) {
+
+let = parseInt(prompt('Escribe tu nombre')); 
+    const nombre1 = "Ana";
+    const nombre2 = "Valeria";
+    const nombre3 = "Lorena";
+    const nombre4 = "Jessica";
+    const nombre5 = "Mario";
+    const nombre6 = "Martín";
+    console.log(nombre1);
+    console.log(nombre2);
+    console.log(nombre3);
+    console.log(nombre4);
+    console.log(nombre5);
+    console.log(nombre6);
+     alert('Vamos a empezar' );
+   
+/*_______________________________________2 ENTREGA______________________/*
+  // let opcion = parseInt(prompt('Seleciona una opcion: 1-sumar o 2-restar o 5-Salir'));
+  // switch (opcion) {
+  //   case 1:
+  //     num1 = parseInt(prompt('Ingrese el 1er numero: '));
+  //     num2 = parseInt(prompt('Ingrese el 2do numero: '));
+  //     console.log(num1 + ' + ' + num2 + ' = ' + suma(num1 + num2));
+  //     break;
+  //   case 2:
+  //     num1 = parseInt(prompt('Ingresa el primer numero: '));
+  //     num2 = parseInt(prompt('Ingresa el segundo numero: '));
+  //     console.log(num1 + ' - ' + num2 + ' = ' + (num1 - num2));
+  //     break;
+  //   case 3:
+  //     ejecutar = false;
+  //     break;
+  //   default:
+  //     alert('Ingresaste una opcion erronea, ingresa otra vez');
+  
+  
+  
+  
+  //const array = [1, 2, 3, 4];
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) {
+   sum += array[i]
+
+ console.log(sum);
+// let nombre = prompt('PARA CONTINUAR INGRESE SU NOMBRE: ');
+// confirm('Hola ' + nombre + '!');
+
+//  let nombre = prompt('PARA CONTINUAR INGRESE SU NOMBRE: ');
+// sconfirm('Hola ' + nombre + '!');
+//  let ejecutar = true;
+// let num1 = 0;
+// let num2 = 0;
+// while (ejecutar) {
+//   let opcion = parseInt(prompt('Seleciona una opcion: 1-sumar o 2-restar o 5-Salir'));
+//   switch (opcion) {
+//     case 1:
+//       num1 = parseInt(prompt('Ingrese el 1er numero: '));
+//       num2 = parseInt(prompt('Ingrese el 2do numero: '));
+//       console.log(num1 + ' + ' + num2 + ' = ' + suma(num1 + num2));
+//       break;
+//     case 2:
+//       num1 = parseInt(prompt('Ingresa el primer numero: '));
+//       num2 = parseInt(prompt('Ingresa el segundo numero: '));
+//       console.log(num1 + ' - ' + num2 + ' = ' + (num1 - num2));
+//       break;
+//     case 3:
+//       ejecutar = false;
+//       break;
+//     default:
+//       alert('Ingresaste una opcion erronea, ingresa otra vez');
+//   }
+
+// let total=0,numeros = [0,1, 2, 3, 4, 5,6,7,8,9,10];
+// numeros.forEach(function(a){total += a;});
+// console.log(total);
+
+//let moneda = prompt("Que tipo de conversión de moneda desea hacer")
+ /* 
+//estructura de una funcion con parametros
+function nombreFuncion(parm1, parm2, parmn) {
+  // codigo a ejecutar por la funcion
+}
+
+function sumar(numero1, numero2) {
+  alert(numero1 + numero2);
+}
+let num1 = parseInt(prompt("Ingrese el primer numero"));
+let num2 = parseInt(prompt("Ingrese el segundo numero"));
+sumar(num1, num2);
+
+function sumar(x, y) {
+  return x + y;
+}
+let resultado = 9;
+console.log("retorno de la suma", resultado); * /
+
+function saludar(x, y) {
+  console.log("hola");
+  return
+  console.log("chao");
+}
+saludar(); * /
+
+function calculadora(numero1, numero2, operacion) {
+  switch (operacion) {
+    case "+":
+      return numero1 + numero2;
+      break;
+    case "-":
+      return numero1 - numero2;
+      break;
+    case "/":
+      return numero1 / numero2;
+      break;
+    case "*":
+      return numero1 * numero2;
+      break;
+    default:
+      return "operacion no valida";
+      break;
+  }
+}
+let numero1 = parseInt(prompt("ingrese numero 1"));
+let numero2 = parseInt(prompt("ingrese numero 2"));
+let operacion = prompt("Ingrese la operacion");
+let resultado = calculadora(numero1, numero2, operacion);
+alert(resultado);
+
+//SCOPE
+{
+  let mensaje = "Hola";
+}
+console.log(mensaje);
+
+let resultado;
+function sumar(primerNumero, segundoNumero) {
+  resultado = primerNumero + segundoNumero;
+}
+sumar(5, 6);
+//Se puede acceder a la variable resultado porque es global
+console.log(resultado);
+
+function sumar(num1, num2) {
+  let resultado = num1 + num2;
+  return resultado;
+}
+function restar(num1, num2) {
+  let resultado = num1 - num2;
+  return resultado;
+}
+const suma = function (a, b) {
+  return a + b;
+};
+/let suma = (a, b) => a + b;
+console.log(suma(6, 8));
+
+saludo();
+function saludo() {
+}
+const saludo = () => { }
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+const iva = x => x * 0.21;
+let precioProducto = 500;
+let descuento = 50;
+
+let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento);
+console.log(nuevoPrecio);*/
